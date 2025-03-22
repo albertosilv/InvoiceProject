686684216243bc3aa59e26230eff9f0fd9238db2
Alberto Silva <alberto.silva5456@gmail.com>
2025-03-22 15:23:11 -0300

    feat: criacao de model Invoice

--- code-invoice/src/main/java/org/invoice/model/Invoice.java
+++ code-invoice/src/main/java/org/invoice/model/Invoice.java
@@ -1,4 +1,24 @@
 package org.invoice.model;
 
-public class Invoice {
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+@Entity
+public class Invoice extends PanacheEntity {
+    public String number;
+    public LocalDateTime issueDate;
+    public String address;
+    public double totalAmount;
+
+    @ManyToOne
+    public Supplier supplier;
+
+    @OneToMany(mappedBy = "invoice")
+    public List<InvoiceItem> items;
 }
