892a9e7023c2172a5dff5d73e0b18f26546a23a3
Alberto Silva <alberto.silva5456@gmail.com>
2025-03-22 19:17:43 -0300

    refactor: Adicionar validacoes no InvoiceItem Model

--- code-invoice/src/main/java/org/invoice/model/InvoiceItem.java
+++ code-invoice/src/main/java/org/invoice/model/InvoiceItem.java
@@ -2,19 +2,30 @@ package org.invoice.model;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.*;
-
+import jakarta.validation.constraints.*;
 
 @Entity
 public class InvoiceItem extends PanacheEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "O preço unitário é obrigatório")
+    @Positive(message = "O preço unitário deve ser positivo")
+    @Column(nullable = false)
     public double unitPrice;
+
+    @NotNull(message = "A quantidade é obrigatória")
+    @Positive(message = "A quantidade deve ser positiva")
+    @Column(nullable = false)
     public int quantity;
 
-    @ManyToOne
+    @NotNull(message = "O produto é obrigatório")
+    @ManyToOne(optional = false)
     public Product product;
 
-    @ManyToOne
+    @NotNull(message = "A fatura é obrigatória")
+    @ManyToOne(optional = false)
     public Invoice invoice;
 
     public double getTotalAmount() {
