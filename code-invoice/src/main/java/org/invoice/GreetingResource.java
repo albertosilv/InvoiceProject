d93d4475db3bfe0cb35a74126c79b537f1f7b46c
Alberto Silva <alberto.silva5456@gmail.com>
2025-03-22 15:03:25 -0300

    refactor: ajustar dependencias

--- code-invoice/src/main/java/org/invoice/GreetingResource.java
+++ code-invoice/src/main/java/org/invoice/GreetingResource.java
@@ -1,16 +1,16 @@
-package com.invoice;
+package org.invoice;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/")
-public class InvoiceResource {
+@Path("/hello")
+public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from Quarkus REST";
+        return "Hello RESTEasy";
     }
 }
--- code-invoice/src/test/java/org/invoice/GreetingResourceTest.java
+++ code-invoice/src/test/java/org/invoice/GreetingResourceTest.java
@@ -1,4 +1,4 @@
-package com.invoice;
+package org.invoice;
 
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
@@ -7,14 +7,14 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
-class InvoiceResourceTest {
+class GreetingResourceTest {
     @Test
     void testHelloEndpoint() {
         given()
-          .when().get("/")
+          .when().get("/hello")
           .then()
              .statusCode(200)
-             .body(is("Hello from Quarkus REST"));
+             .body(is("Hello RESTEasy"));
     }
 
 }
